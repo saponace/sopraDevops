8a89b229ed9b6f57b6f2c43068e22c340977f511
Rémi Somdecoste* <saponace@gmail.com>
2016-11-08 17:22:57 +0100

    Yo, ajout de la gestion des nombres négatifs

--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -1,20 +1,21 @@
-
-
 public class Function {
-
-
-	public long fibonacci(long rang) {
-		if (rang <= 1){
-			return rang;
-		}
-        else {
-        	return fib(rang-1) + fib(rang-2);
-		}
-	}
-
-	public static void main(String[] args) {
-        int rang = Integer.parseInt(args[0]);
-        System.out.println("Fibonnaci de" + rang + "est" + fibonacci(rang));
+    private static long fibonacci(long rang) {
+	if(rang < 0)
+	    throw new IllegalArgumentException("Le paramètre doit être un entier positif.");
+	if (rang <= 1)
+	    return rang;
+	else
+	    return fibonacci(rang-1) + fibonacci(rang-2);
     }
 
+
+    public static void main(String[] args) {
+	int rang = Integer.parseInt(args[0]);
+	try{
+	    System.out.println("Fibonnaci de " + rang + " est " + fibonacci(rang) + ".");
+	}
+	catch(IllegalArgumentException e){
+	    System.err.println(e.getMessage());
+	}
+    }
 }
